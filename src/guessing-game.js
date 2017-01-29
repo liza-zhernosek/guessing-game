beea13d783a973d96a25ae371ada3e4537d10dda
Liza Zhernosek <liza.zhernosek@bk.ru>
2017-01-29 23:45:27 +0300

    guessing-game/methods: implement entire game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.current = Math.ceil((this.min + this.max) / 2);
+      return this.current;
     }
 
     lower() {
-
+      this.max = this.current;
     }
 
     greater() {
-
+      this.min = this.current;
     }
 }
 
